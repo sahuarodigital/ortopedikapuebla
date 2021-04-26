e41558fee275e63f5f630ff099bc12d70617aa6d
shirrisn <iriswilsonr@gmail.com>
2021-04-25 23:50:37 -0500

    mejoras

---
+++ medical/js/custom.js
@@ -0,0 +1,16 @@
+
+function fichaTecnica(doctor){
+    debugger;
+    if(doctor ==1){
+        var link = document.getElementById('especialistaCoca');
+        link.click();
+    }
+    if(doctor == 2){
+        var link = document.getElementById('especialistaIvan');
+        link.click();
+    }
+    if(doctor == 3){
+        var link = document.getElementById('especialistaAndy');
+        link.click();
+    }
+}
